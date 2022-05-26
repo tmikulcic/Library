93d67afabee854c2623060880a35705a5ecd2d9c
Tomislav <tom.mikulcic@gmail.com>
2022-05-26 13:50:42 +0200

    Add functionality to store Book data in Library array

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let button = document.querySelector('#btn');
+button.addEventListener('click', addBookToLibrary);
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -7,4 +9,16 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  let author = document.querySelector('#author').value;
+  let title = document.querySelector('#title').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read');
+  console.log(author);
+  console.log(title);
+  console.log(pages);
+  console.log(read.checked);
+
+  myLibrary.push({ author, title, pages, read });
+  console.table(myLibrary);
+}
