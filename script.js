674e0e20a5a74e9ebaac419ad23c3cfdbbee1b57
Tomislav <tom.mikulcic@gmail.com>
2022-05-26 13:23:43 +0200

    Inital HTML layout, JS code and CSS styling

---
+++ script.js
@@ -0,0 +1,10 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {}
